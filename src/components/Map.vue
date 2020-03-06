4dd899716348a25fa1279a967c6346fa76016041
Tris <tgfc059@tgfc.tw>
2020-03-06 18:00:19 +0800

    【新增】leaflet地圖

---
+++ src/components/Map.vue
@@ -0,0 +1,42 @@
+<script>
+import { LMap, LTileLayer } from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
+export default {
+  props: {},
+  mounted() {
+  },
+  data() {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      zoom: 17,
+      center: [24.165348479759157, 120.6325650215149],
+      bounds: null
+    }
+  },
+  methods: {
+    zoomUpdated (zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated (center) {
+      this.center = center;
+    }
+  },
+  render(){
+    return (
+        <div style="height: 100%; width: 100%;">
+          <LMap
+            style="height: 80%"
+            zoom={this.zoom}
+            center={this.center}
+          >
+            <LTileLayer url={this.url}></LTileLayer>
+          </LMap>
+        </div>
+
+    )
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div style="height:100%" class="home">
+    <Map style="height:100%" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Map from "@/components/Map";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Map
   }
 };
 </script>
